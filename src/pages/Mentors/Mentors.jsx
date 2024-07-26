ed8ef3647c2bf578669b69bb46dafdc3b3e94b9d
Sultan <tamaevsultan@gmail.com>
2024-07-26 17:41:08 +0600

    mentors done completely

--- src/pages/Mentors/Mentors.jsx
+++ src/pages/Mentors/Mentors.jsx
@@ -12,37 +12,41 @@ export const Mentors = () => {
 
          <div className="mentors-blocks">
             <div className="mentors-block">
-					<div className="mentors-img"></div>
-					<div className="mentor-details">
-						<div className="mentor-descr">
-							<div className="mentors-name">Ахмад Мединский</div>
-							<div className="mentors-text">Я учитель Корана, стремящийся передать своим <br />ученикам любовь и уважение к Священному <br />Писанию.</div>
-							
-							<div className="mentors-info">
-								<div className="info-item">
-									<span className="info-label">Провел уроков на платформе: </span>
-									<span className="info-value info-value-1">215</span> {/* when i get api i'll write this like a variable */}
-								</div> 
-								<div className="info-item">
-									<span className="info-label">Опыт:</span>
-									<span className="info-value">10 лет</span> {/* same */}
-								</div>
-								
+				<div className="mentors-img"></div>
+				<div className="mentor-details">
+					<div className="mentor-descr">
+						<div className="mentors-descr-1-column">
+							<div className="mentors-name-text">
+								<div className="mentors-name">Ахмад Мединский</div>
+								<div className="mentors-text">Я учитель Корана, стремящийся передать своим <br />ученикам любовь и уважение к Священному <br />Писанию.</div>
 							</div>
-							<div className="info-item">
-								<span className="info-price">500₽ / в час</span> {/*same */}
+							
+							
+							<div className="mentors-tags-buttons">
+								<div className="mentors-tags">
+									<button className="mentors-tag-button">Таджвид</button>
+									<button className="mentors-tag-button">Махрадж</button>
+									<button className="mentors-tag-button">и еще 9...</button>
+								</div>
 							</div>
 						</div>
-               </div>
+						
 
-					<div className="mentors-tags-buttons">
-                  <div className="mentors-tags">
-                     <button className="mentors-tag-button">Таджвид</button>
-                     <button className="mentors-tag-button">Махрадж</button>
-                     <button className="mentors-tag-button">и еще 9...</button>
-                  </div>
-            	</div>
-                        
+						<div className="mentors-info">
+							<div className="info-item">
+								<span className="info-label info-label-icon-1">Провел уроков на платформе: </span>
+								<span className="info-value">215</span> {/* when i get api i'll write this like a variable */}
+							</div> 
+							<div className="info-item">
+								<span className="info-label info-label-icon-2">Опыт:</span>
+								<span className="info-value">10 лет</span> {/* same */}
+							</div>
+						</div>
+						<div className="info-item">
+							<span className="info-price">500₽ / в час</span> {/*same */}
+						</div>
+					</div>
+				</div>
             </div>
          </div>
       </div>

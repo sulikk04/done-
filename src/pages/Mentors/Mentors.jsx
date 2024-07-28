7d6528e64bad0cfbb67babc3ecea08659227d103
Sultan <tamaevsultan@gmail.com>
2024-07-28 06:30:46 +0600

    fixed everything!

--- src/pages/Mentors/Mentors.jsx
+++ src/pages/Mentors/Mentors.jsx
@@ -44,6 +44,105 @@ export const Mentors = () => {
 					</div>
 				</div>
             </div>
+            <div className="mentors-block">
+				<div className="mentors-img"></div>
+				<div className="mentor-details">
+					<div className="mentor-descr">
+						<div className="mentors-descr-1-column">
+							<div className="mentors-name-text">
+								<div className="mentors-name">Ахмад Мединский</div>
+								<div className="mentors-text">Я учитель Корана, стремящийся передать своим <br />ученикам любовь и уважение к Священному <br />Писанию.</div>
+							</div>
+							
+							
+							<div className="mentors-tags">
+								<button className="mentors-tag-button">Таджвид</button>
+								<button className="mentors-tag-button">Махрадж</button>
+								<button className="mentors-tag-button">и еще 9...</button>
+							</div>
+						</div>
+						
+
+						<div className="mentors-info">
+							<div className="info-item">
+								<span className="info-label info-label-icon-1">Провел уроков на платформе: <span className='info-value'>215</span> </span>
+							</div> 	
+							<div className="info-item">
+								<span className="info-label info-label-icon-2">Опыт: <span className='info-value'>10 лет</span></span>
+							</div>
+						</div>
+						<div className="info-price-wrapper">
+							<span className="info-price">500₽ / в час</span> {/*same */}
+						</div>
+					</div>
+				</div>
+            </div>
+            <div className="mentors-block">
+				<div className="mentors-img"></div>
+				<div className="mentor-details">
+					<div className="mentor-descr">
+						<div className="mentors-descr-1-column">
+							<div className="mentors-name-text">
+								<div className="mentors-name">Ахмад Мединский</div>
+								<div className="mentors-text">Я учитель Корана, стремящийся передать своим <br />ученикам любовь и уважение к Священному <br />Писанию.</div>
+							</div>
+							
+							
+							<div className="mentors-tags">
+								<button className="mentors-tag-button">Таджвид</button>
+								<button className="mentors-tag-button">Махрадж</button>
+								<button className="mentors-tag-button">и еще 9...</button>
+							</div>
+						</div>
+						
+
+						<div className="mentors-info">
+							<div className="info-item">
+								<span className="info-label info-label-icon-1">Провел уроков на платформе: <span className='info-value'>215</span> </span>
+							</div> 	
+							<div className="info-item">
+								<span className="info-label info-label-icon-2">Опыт: <span className='info-value'>10 лет</span></span>
+							</div>
+						</div>
+						<div className="info-price-wrapper">
+							<span className="info-price">500₽ / в час</span> {/*same */}
+						</div>
+					</div>
+				</div>
+            </div>
+            <div className="mentors-block">
+				<div className="mentors-img"></div>
+				<div className="mentor-details">
+					<div className="mentor-descr">
+						<div className="mentors-descr-1-column">
+							<div className="mentors-name-text">
+								<div className="mentors-name">Ахмад Мединский</div>
+								<div className="mentors-text">Я учитель Корана, стремящийся передать своим <br />ученикам любовь и уважение к Священному <br />Писанию.</div>
+							</div>
+							
+							
+							<div className="mentors-tags">
+								<button className="mentors-tag-button">Таджвид</button>
+								<button className="mentors-tag-button">Махрадж</button>
+								<button className="mentors-tag-button">и еще 9...</button>
+							</div>
+						</div>
+						
+
+						<div className="mentors-info">
+							<div className="info-item">
+								<span className="info-label info-label-icon-1">Провел уроков на платформе: <span className='info-value'>215</span> </span>
+							</div> 	
+							<div className="info-item">
+								<span className="info-label info-label-icon-2">Опыт: <span className='info-value'>10 лет</span></span>
+							</div>
+						</div>
+						<div className="info-price-wrapper">
+							<span className="info-price">500₽ / в час</span> {/*same */}
+						</div>
+					</div>
+				</div>
+            </div>
          </div>
       </div>
     </div>
